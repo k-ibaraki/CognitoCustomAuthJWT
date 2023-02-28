adefeabf018ac6cc33b1fa20b767a6a33d449f82
k-ibaraki <ibaraki@ncdc.co.jp>
2023-02-28 20:21:48 +0900

    add .node-version

--- lambda/serverless.ts
+++ lambda/serverless.ts
@@ -2,11 +2,11 @@ import { Serverless } from 'serverless/aws';
 import { config } from "dotenv";
 config(); // .envファイルを読み込む
 
-const awsProfile = process.env.AWS_PROFILE
+const awsProfile = process.env.AWS_PROFILE;
 const userPoolName = process.env.USER_POOL_NAME ?? "";
 const userPoolExisting = (process.env.USER_POOL_EXISTING == 'true');
 const slsServiceName = process.env.SLS_SERVICE_NAME ?? "";
-const jwtSecret = process.env.JWT_SECRET ?? ""
+const jwtSecret = process.env.JWT_SECRET ?? "";
 
 const serverlessConfiguration: Serverless = {
   service: slsServiceName,
